c666636390b01b470bc57c959287a2a4b0787fba
Fahlevi20 <rozakalfi@gmail.com>
2024-02-13 09:40:56 +0700

    first commit

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Box, Center } from "@chakra-ui/react";
+
+export default function NavBar (){
+    return(
+        <Box p={5} textAlign={"Center"}>
+            This is For sidebar
+
+        </Box>
+
+    )
+}
