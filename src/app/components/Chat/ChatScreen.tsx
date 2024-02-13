bc803f8169ff2806b3cd25bc3a7ad4bdc3328546
Fahlevi20 <rozakalfi@gmail.com>
2024-02-13 11:15:48 +0700

    fix output colm

--- src/app/components/Chat/ChatScreen.tsx
+++ src/app/components/Chat/ChatScreen.tsx
@@ -4,7 +4,7 @@ export default function ChatScreen() {
     return(
         <Box backgroundColor={"black"} width='100%' height={800} p={100}>
             this is chatscreen
-
+//input text
             <Box p={5} backgroundColor={"green"} 
             width={"fit-content"} 
             border='2px' borderColor='gray.200' 
@@ -13,7 +13,16 @@ export default function ChatScreen() {
             // borderColor="white.200" 
             // borderRadius={2}
             >
-                this is input message
+                Hi
+            </Box>
+            <Box display='flex' p={5} backgroundColor={"darkgreen"} 
+            width={"fit-content"} 
+            border='2px' borderColor='gray.200' 
+            boxShadow={"xs"}
+            rounded="lg"
+            >
+                Hello Bro!
+                
             </Box>
         </Box>
     )
