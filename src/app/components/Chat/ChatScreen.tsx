f290d9fc743d16059a7e962543281fb3a65d66ff
Fahlevi20 <rozakalfi@gmail.com>
2024-02-13 11:05:36 +0700

    add input text fronted and border

--- src/app/components/Chat/ChatScreen.tsx
+++ src/app/components/Chat/ChatScreen.tsx
@@ -2,8 +2,19 @@ import { Box, DarkMode } from "@chakra-ui/react";
 
 export default function ChatScreen() {
     return(
-        <Box backgroundColor={"blue"} width='100%' height={800} p={100}>
+        <Box backgroundColor={"black"} width='100%' height={800} p={100}>
             this is chatscreen
+
+            <Box p={5} backgroundColor={"green"} 
+            width={"fit-content"} 
+            border='2px' borderColor='gray.200' 
+            boxShadow={"xs"}
+            rounded="lg"
+            // borderColor="white.200" 
+            // borderRadius={2}
+            >
+                this is input message
+            </Box>
         </Box>
     )
 }
