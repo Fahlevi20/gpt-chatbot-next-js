5733fec8fda217a9a66e5d01e2be0440a32f66c8
Fahlevi20 <rozakalfi@gmail.com>
2024-02-16 08:27:18 +0700

    add handlesubmit and hitting api using axios

--- src/app/components/Chat/InputChat.tsx
+++ src/app/components/Chat/InputChat.tsx
@@ -1,4 +1,18 @@
 import { Box, Button, Flex, Input } from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
+
+
+const [input,setInput] = useState("");
+const[output, setOutput] = useState("");
+
+const handleSubmit = async (e) => {
+    e.preventDeault();
+}
+
+const response = await axios.post('http://127.0.0.1:5000/chat_gpt', {
+    
+})
 
 export default function InputChat(){
     return (
