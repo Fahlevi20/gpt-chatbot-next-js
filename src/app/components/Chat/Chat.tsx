064ee840e39adfdceab235b4e3d0c68a137e7bd4
Fahlevi20 <rozakalfi@gmail.com>
2024-02-16 19:58:30 +0700

    add logic for input and output

--- src/app/components/Chat/Chat.tsx
+++ src/app/components/Chat/Chat.tsx
@@ -1,14 +1,31 @@
 import { Box, Button, DarkMode, Flex, HStack, Input, Text } from "@chakra-ui/react";
 import InputChat from "./InputChat";
 import { useState } from "react";
+import axios from "axios";
 
-const [input,setInput] = useState(""); //untuk menyimpan input
-const [output,setOutput] = useState(""); //untuk menyimpan output
+
 
 
 
 export  default function Chat() {
+    const [input,setInput] = useState("hello"); //untuk menyimpan input
+const [output,setOutput] = useState("yes"); //untuk menyimpan output
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("http://127.0.0.1:5000/chat_gpt", 
+    {"user": "user",
+    "message":input,
+}, {
+    headers: {
+        'Content-Type':"application/json"
+    }
+});
+    setOutput(response.data);
+};
     return (
+
+        
         <Box>
         <Flex direction={"column"} backgroundColor={'darkgray'} height={564}>
             <Flex backgroundColor={'green'} 
@@ -18,8 +35,9 @@ export  default function Chat() {
             rounded={'md'} 
             minH={5} 
             alignSelf={"flex-start"}>
-                <Text>Hello</Text>
+                <Text>{input}</Text>
             </Flex>
+            
             <Flex backgroundColor={'green'} 
             color={'lightgray'} 
             width={'fit-content'} 
@@ -27,7 +45,7 @@ export  default function Chat() {
             rounded={'md'} 
             minH={5} 
             alignSelf={"flex-end"}>
-                <Text>Hello</Text>
+                <Text>{output}</Text>
             </Flex>
 
         </Flex>
