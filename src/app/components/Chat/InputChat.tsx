636028a729b82f0606783a270c0a59c3e2aa3325
Fahlevi20 <rozakalfi@gmail.com>
2024-02-16 08:28:51 +0700

    add parameter to api

--- src/app/components/Chat/InputChat.tsx
+++ src/app/components/Chat/InputChat.tsx
@@ -11,7 +11,8 @@ const handleSubmit = async (e) => {
 }
 
 const response = await axios.post('http://127.0.0.1:5000/chat_gpt', {
-    
+    "User":"user",
+    "message":input
 })
 
 export default function InputChat(){
